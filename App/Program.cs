176043951b6dbc1741d20d00d14ec203b1028ab0
Abdulkhader, ShoukatAli <ShoukatAli.Abdulkhader@allscripts.com>
2021-05-30 22:24:34 -0400

    checked in gitignore file

---
+++ App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length >= 1)
+            {
+                Console.WriteLine("Hello " + args[0] + " World!");
+            }
+            else
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
